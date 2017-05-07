d528f4eff5e9ec202a777565bab439a7c1d78fe8
Deepakkv <deepakkv@in-deepakkv.local>
2017-05-07 20:00:52 +0530

    Multiple markers with infowindow and bounds.

--- js/index.js
+++ js/index.js
@@ -1,25 +1,50 @@
 var map;
 var marker;
-var myLatLng = {lat: 12.969405, lng: 77.559909};
+var myLatLng = {lat: 40.7713024, lng: -73.9632393};
 var infoWindow;
-var content = 'Home';
+var locations = [
+    {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+    {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+    {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+    {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+    {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+    {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+];
+var markers = [];
+var infoWindows = [];
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center : myLatLng,
-        zoom: 15
+        zoom: 12
     });
 
-    marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Home'
-    });
+    largeInfoWindow = new google.maps.InfoWindow();
+    bounds = new google.maps.LatLngBounds();
 
-    infoWindow = new google.maps.InfoWindow({
-        content: content
-    });
+    for (let i =0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: locations[i].location,
+            title: locations[i].title,
+            map: map,
+            id: i
+        });
+        markers.push(marker)
+        bounds.extend(marker.position)
 
-    marker.addListener('click', function () {
-        infoWindow.open(map, marker);
-    });
+        marker.addListener('click', function () {
+            populateInfoWindow(this, largeInfoWindow);
+        });
+    }
+    map.fitBounds(bounds);
+}
+
+function populateInfoWindow(marker, infoWindow) {
+    if (infoWindow.marker != marker) {
+        infoWindow.marker = marker;
+        infoWindow.setContent('<div>' + marker.title + '<div>');
+        infoWindow.open(map, marker)
+        infoWindow.addListener('closeClick', function () {
+            infoWindow.setMarker(null);
+        })
+    }
 }
