7e7f45fb4e5bfffae59a1a94da0d7f06f468b7bb
Deepakkv <deepakkv@in-deepakkv.local>
2017-05-07 17:44:10 +0530

    InfoWindow on marker.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 var map;
 var marker;
 var myLatLng = {lat: 12.969405, lng: 77.559909};
+var infoWindow;
+var content = 'Home';
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center : myLatLng,
@@ -11,5 +13,13 @@ function initMap() {
         position: myLatLng,
         map: map,
         title: 'Home'
+    });
+
+    infoWindow = new google.maps.InfoWindow({
+        content: content
+    });
+
+    marker.addListener('click', function () {
+        infoWindow.open(map, marker);
     });
 }
