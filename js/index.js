be7750473d67b1bfe7e0bf6a0353764ff086c00a
Deepakkv <deepakkv@in-deepakkv.local>
2017-05-07 16:31:02 +0530

    Added marker on Map to set Home loccation(Manual coordinates)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 var map;
+var marker;
+var myLatLng = {lat: 12.969405, lng: 77.559909};
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center : {lat: -34.387, lng: 150.644},
-        zoom: 8
+        center : myLatLng,
+        zoom: 15
+    });
+
+    marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Home'
     });
 }
